8dee9bafe1c9c35e988b85327d35bac49fd9c02c
riddle-bash <hainam2016@gmail.com>
2024-02-17 09:28:20 +0700

    Hibernate product entity

---
+++ src/main/java/com/example/testEstore/models/ProductDto.java
@@ -0,0 +1,5 @@
+package com.example.testEstore.models;
+
+public class ProductDto {
+
+}
---
+++ src/main/java/com/example/testEstore/models/ProductEntity.java
@@ -0,0 +1,74 @@
+package com.example.testEstore.models;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "products")
+public class ProductEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String name;
+	private String brand;
+	private String category;
+	private double price;
+	private String description;
+	private String imageFileName;
+	private Date timeCreated;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getImageFileName() {
+		return imageFileName;
+	}
+	public void setImageFileName(String imageFileName) {
+		this.imageFileName = imageFileName;
+	}
+	public Date getTimeCreated() {
+		return timeCreated;
+	}
+	public void setTimeCreated(Date timeCreated) {
+		this.timeCreated = timeCreated;
+	}
+}
