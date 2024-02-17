159ceb4c59215c1008b4743e64ed9a503e1cdc62
riddle-bash <hainam2016@gmail.com>
2024-02-17 09:09:22 +0700

    Intial Spring

---
+++ src/main/java/com/example/testEstore/TestEstoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.testEstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestEstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestEstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testEstore/TestEstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testEstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestEstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
